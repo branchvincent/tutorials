757eebc230893a9383347510be2b2b6caeddfb54
Branch Vincent <branchevincent@gmail.com>
2021-10-05 23:29:30 -0400

    fix

--- python/fastapi/tests/test_openapi.py
+++ python/fastapi/tests/test_openapi.py
@@ -4,10 +4,9 @@ from app.main import app
 
 schemathesis.fixups.install()
 
-# FIXME: Spec fails schema validation
-# schema = schemathesis.from_dict(app.openapi())
+schema = schemathesis.from_dict(app.openapi(), validate_schema=False)
 
 
-# @schema.parametrize()
-# def test_api(case: schemathesis.Case) -> None:
-#     case.call_and_validate()
+@schema.parametrize()
+def test_api(case: schemathesis.Case) -> None:
+    case.call_and_validate()
